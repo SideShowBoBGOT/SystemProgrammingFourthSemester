ea45644d9de792a2c54a6481d2f28afbe5559c54
sideshowbobgot <sideshowbobgot@gmail.com>
2023-02-16 01:10:44 +0200

    lab 2 in progress

--- lab2/lab2/lab.asm
+++ lab2/lab2/lab.asm
@@ -1,29 +1,29 @@
 bits 64
 
-; list of system calls
-; https://filippo.io/linux-syscall-table/
-SYS_READ equ 0
-SYS_WRITE equ 1
+;   list of system calls
+;   https://filippo.io/linux-syscall-table/
+SYS_READ    equ 0
+SYS_WRITE   equ 1
 
-; descriptors
-STDIN equ 0
-STDOUT equ 1
+;   Descriptors
+STDIN   equ 0
+STDOUT  equ 1
 
-; special characters
-NEW_LINE_CHARACTER equ 10
-NULL_TERMINATOR equ 0
+;   ASCII characters
+NULL_TERMINATOR     equ 0
+NEW_LINE_CHARACTER  equ 10
+DIGIT_ZERO          equ 48
+DIGIT_NINE          equ 57
 
-; other constants
-BUFFER_LENGTH equ 5
+;   Other constants
+BUFFER_LENGTH   equ 5
+
+
 section .data
-error_incorrect_symbol: db "Incorrect symbol in input", 0
-buffer: times BUFFER_LENGTH db 0
-inputtedLength: dq 0
+error_incorrect_symbol:                         db  "Incorrect symbol in input", 0
+buffer:                     times BUFFER_LENGTH db  0
+inputtedLength:                                 dq  0
 
-;   why rcx and r11 are modified after system calls?
-;   https://stackoverflow.com/questions/47983371/why-do-x86-64-linux-system-calls-modify-rcx-and-what-does-the-value-mean
-;   http://www.int80h.org/bsdasm/#system-calls
-;   https://docs.freebsd.org/en/books/developers-handbook/x86/#x86-system-calls
 section .text
 global asm_main
 asm_main:
@@ -36,7 +36,7 @@ asm_main:
     mov rdi, BUFFER_LENGTH
     call ReadIntoBuffer
     mov rsi, [rsi]
-    call ConvertStringToInteger
+    call TryConvertStringToInteger
 
     pop rdx
     pop rsi
@@ -51,7 +51,7 @@ ReadIntoBuffer:
 ;   Params:
 ;       rax:    char*   buffer
 ;       rdi:    int     bufferLength
-;       rsi:    int     inputtedLength
+;       rsi:    int*    inputtedLength
 ;   Returns:
 ;       void
     push rax
@@ -69,6 +69,11 @@ ReadIntoBuffer:
 
     call DoSystemCallNoModify
 
+    cmp rax, rdx
+    jle .End
+    mov rax, rdx
+
+    .End:
     mov rsi, r8
     mov [rsi], rax
 
@@ -84,14 +89,17 @@ DoSystemCallNoModify:
 ;   The reason behind this function is that in x64 NASM
 ;   system call neither stores nor loads any registers
 ;   it just uses and modifies them.
+
 ;   https://stackoverflow.com/questions/47983371/why-do-x86-64-linux-system-calls-modify-rcx-and-what-does-the-value-mean
+;   http://www.int80h.org/bsdasm/#system-calls
+;   https://docs.freebsd.org/en/books/developers-handbook/x86/#x86-system-calls
     push rcx
     push r11
     syscall
     pop r11
     pop rcx
     ret
-ConvertStringToInteger:
+TryConvertStringToInteger:
 ;   Function converting string to integer;
 ;   bool ReadIntoBuffer(char* buffer, int bufferLength, int inputtedLength, int* number);
 ;   Params:
@@ -104,60 +112,54 @@ ConvertStringToInteger:
 ;
     push rbx
     push rcx
-    push rdx
-    push r8
 
-    ; int index = 0;
     xor rcx, rcx
-    ; r8 = &buffer
-    mov r8, buffer
-
-    ; if rax > BUFFER_LENGTH go to A1
-    cmp rax, BUFFER_LENGTH
-    jb .A1
-        ; <<A1 false part>>
-        ; <<A1 true part>>
-        mov dl, byte [r8+rcx]
-        ; check whether dl is
-        ; in range of [48; 57]
-        ; '0' has code of 48
-        ; '9' has code of 57 in ASCII
-
-
-
-        ; increment counter
-        inc rcx
-        cmp rcx, rax
-        jl A1
-        jp End
+    ; if inputtedLength > bufferLength
+    cmp rsi, rdi
+    je .A2
     .A1:
-        ; <<A1 true part>>
-        ; read byte element from buffer
-        mov dl, byte [r8+rcx]
-        ; check whether dl is
-        ; in range of [48; 57]
-        ; '0' has code of 48
-        ; '9' has code of 57 in ASCII
+        .A1B1Loop:
 
+        jl .A1B1Loop
+        jp .End
+    .A2:
+        .A2B1Loop:
 
-
-        ; increment counter
-        inc rcx
-        cmp rcx, rax
-        jl A1
-        jp End
+        jl .A2B1Loop
+        jp .End
     .ErrorIncorrectSymbol
+        mov r8, 0
+        jmp .End
 
+    .NoError:
+        mov r8, 1
+        jmp .End
     .End:
-
-    pop r8
-    pop rdx
     pop rcx
     pop rbx
 
     ret
-
-
+IsDigit:
+;   Function checking whether byte value
+;   is in digit codes range
+;   bool IsDigit(char c);
+;   Params:
+;       rax:    char    c
+;   Returns:
+;       rdi:    bool    if true, then it is digit, else not
+    cmp rax, DIGIT_ZERO
+    jl .False
+    cmp rax, DIGIT_NINE
+    jb .False
+    jmp .True
+    .False:
+        mov rdi, 0
+        jmp .End
+    .True:
+        mov rdi, 1
+        jmp .End
+    .End:
+    ret
 
 
 
